e0cdb59f4f670428793e0a6fbd81134b664f034c
Yim-HaEun <49932613+Yim-HaEun@users.noreply.github.com>
2023-12-19 11:39:32 +0900

    animal

--- src/js/TimerCount.js
+++ src/js/TimerCount.js
@@ -5,12 +5,16 @@ function Timer(){
     useEffect(()=>{
         document.title = `타이틀을 보셔라${count}`;
     });
+  
     return(
         <div>
             <p>{count}</p>
-            <button onClick={()=> setCount(count +1)}>
-                Click
-            </button>
+            <button onClick={()=> setCount(count +1)}>Click</button>
+            {/* <button onClick={increment}>Click</button>*/}
+            {/*const increment = () => {
+            setCount(count + 1);
+             } 위 버튼 클릭과 const incremanet로 작성해준 메서드는 동일한 효능을 지닌다*/}
+
         </div>
     )
 }export default Timer;
